9b13249b879435303c3f27c78ca7e00060b9f756
mengistulemma <tsehaymelaku@hotmail.com>
2025-07-17 14:22:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Mytotalav'
+project = 'Totalvhelpdocs'
 copyright = '2025'
-author = 'Leonhard Fie'
+author = 'Teshome Rumi'
 
 release = '1.0'
 
